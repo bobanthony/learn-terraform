120e0a1439056605ffb4423b1f3950fcce08cf03
Dinesh <satyadineshk328@gmail.com>
2023-02-25 21:42:45 +0530

    gu

--- data source.sh/ami.tf
+++ data source.sh/ami.tf
@@ -1,10 +1,12 @@
+
 data "aws_ami" "ami" {
 
-  most_recent      = true
-  name_regex       = "centos-8-DevOps-practice"
-  owners           = ["973714476881"]
+  most_recent = true
+  name_regex  = "centos-8-DevOps-practice"
+  owners      = ["973714476881"]
+}
+   output "ami_id" {
+     value = data.aws_ami.ami.image_id
+   }
 
-}
-  output "ami_id" {
-  value = data.aws_ami.ami.image_id
-}
+
