adef3e114829f9eb2368588d9bc1c6d09938573d
Dinesh <satyadineshk328@gmail.com>
2023-03-04 11:08:49 +0530

    updating ./learn-terraform

--- 01 sample/test.tf
+++ 01 sample/test.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "trie" {
   ami = "ami-0a017d8ceb274537d"
   vpc_security_group_ids = ["sg-0b173a0934aeda686"]
+  instance_type = "t2.micro"
   tags = {
     name = "test"
   }
