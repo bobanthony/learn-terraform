dd97f7a45f52da787d64f28312f038478f359c72
Dinesh <satyadineshk328@gmail.com>
2023-03-04 12:36:26 +0530

    updating ./learn-terraform

--- data source/ami.tf
+++ data source/ami.tf
@@ -1,12 +1,3 @@
 
-data "aws_ami" "ami" {
-
-  most_recent = true
-  name_regex  = "centos-8-DevOps-practice"
-  owners      = ["973714476881"]
-}
-   output "ami_id" {
-     value = data.aws_ami.ami.id
-   }
 
 
