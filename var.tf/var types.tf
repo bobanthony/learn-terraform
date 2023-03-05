9dbb49bae24a1b5d6417717ce045f11c14e3cbc6
Dinesh <satyadineshk328@gmail.com>
2023-03-05 11:49:28 +0530

    updating ./learn-terraform

---
+++ var.tf/var types.tf
@@ -0,0 +1,26 @@
+variable "example1" {
+  default = "Hello world"
+}
+
+variable "example2" {
+  default = [
+    "Good day",
+    1997,
+    true,
+    "harleen deol"
+  ]
+}
+
+variable "example3" {
+  default = {
+    strings = "deol",
+    numbers = 21,
+    boolean = true
+
+  }
+}
+
+output "types" {
+  value = "variable example1 - ${var.example1}, first value in the list - ${var.example2[0]}", boolean value of the map - ${var.example3["boolean"]}
+
+}
