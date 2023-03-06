249c8f921b178a0db6fc9f45168cefcf0ecc3d8b
Dinesh <satyadineshk328@gmail.com>
2023-03-06 18:01:36 +0530

    updating ./learn-terraform

--- 05-s3.state/main.tf
+++ 05-s3.state/main.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
     bucket = "learn-devops-online"
-    key = "05-s3-state/terraform.tfstate"
+    key    = "05-s3-state/terraform.tfstate"
     region = "us-east-1"
   }
 }
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0b173a0934aeda686"]
   tags = {
     name = "match"
