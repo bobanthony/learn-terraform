18e41bf614ee78fc7eb0267ef8f96d64055961b2
Dinesh <satyadineshk328@gmail.com>
2023-03-06 19:46:27 +0530

    updating ./learn-terraform

--- roboshop set-up/ex2/main.tf
+++ roboshop set-up/ex2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  count                  = 5
+  count                  = length(var.instances)
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0b173a0934aeda686"]
