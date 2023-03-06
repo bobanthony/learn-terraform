4acbf447f7268adfbfbc13172e37a13cf7b4bd72
Dinesh <satyadineshk328@gmail.com>
2023-03-06 20:24:40 +0530

    updating ./learn-terraform

--- roboshop set-up/ex2/main.tf
+++ roboshop set-up/ex2/main.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "frontend" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0b173a0934aeda686"]
+  tags = {
+    name = var.instances[count.index]
+  }
 }
 variable "instances" {
   default = ["cart","catalogue","shipping","payment" ]
