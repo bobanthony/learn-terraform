1055065a414cc5774b004adcf1b6189b6068e674
Dinesh <satyadineshk328@gmail.com>
2023-03-08 15:51:48 +0530

    updating ./learn-terraform

--- 04-conditions/main.tf
+++ 04-conditions/main.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "deol" {
 variable "instance_type" {}
 
 resource "aws_instance" "harleen" {
-  count         = tobool(var.instances) ? 1 : 0
+  count         = tobool(var.create_instances) ? 1 : 0
   ami           = "ami-0a017d8ceb274537d"
   instance_type = var.instance_type == "" ?  "t3.micro" : var.instance_type
 }
- variable "instances" {}
+ variable "create_instances" {}
