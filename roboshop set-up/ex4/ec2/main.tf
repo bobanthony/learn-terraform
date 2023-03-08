0b5129c5b0f24d2375052b5f8a919adebb6f69ea
Dinesh <satyadineshk328@gmail.com>
2023-03-08 19:18:38 +0530

    updating ./learn-terraform

--- roboshop set-up/ex4/ec2/main.tf
+++ roboshop set-up/ex4/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0b173a0934aeda686"]
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [var.sg_id]
   tags = {
     name = "var.component"
   }
@@ -14,3 +14,5 @@ resource "aws_route53_record" "deol" {
   records = [aws_instance.ec2.private_ip]
 }
 variable "component" {}
+variable "instance_type" {}
+variable "sg_id" {}
