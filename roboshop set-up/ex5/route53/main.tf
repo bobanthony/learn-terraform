7859cdc0f603c6c0fcb9ea528be7b09b9a4711f1
Dinesh <satyadineshk328@gmail.com>
2023-03-09 11:08:25 +0530

    updating ./learn-terraform

--- roboshop set-up/ex5/route53/main.tf
+++ roboshop set-up/ex5/route53/main.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "record" {
   zone_id = "Z00739661SEOHEMKPHEUL"
   name    = "${var.component}-dev.learndevopsb71solutions.site"
   type    = "A"
-  ttl     = 300
+  ttl     = 30
   records = [var.private_ip]
 }
 variable "private_ip" {}
