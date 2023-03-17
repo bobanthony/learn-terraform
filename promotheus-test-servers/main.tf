458196cf5783a0b543e0bf2f8348bebdc978b432
Dinesh <satyadineshk328@gmail.com>
2023-03-17 15:28:36 +0530

    updating ./learn-terraform

--- promotheus-test-servers/main.tf
+++ promotheus-test-servers/main.tf
@@ -15,7 +15,7 @@ resource "aws_ec2_tag" "deol" {
 }
 resource "aws_spot_instance_request" "match" {
   ami           = "ami-0a017d8ceb274537d"
-  vpc_security_group_ids = "[sg-0b173a0934aeda686"]
+  vpc_security_group_ids = ["sg-0b173a0934aeda686"]
   instance_type = "t2.micro"
 
   tags = {
